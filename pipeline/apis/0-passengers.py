3cc24699e46d7a7156c1f016daa5d5437dda903c
nasirk007 <nsr.capital.007@outlook.com>
2026-08-12 01:00:00 +0200

    request and fetch data while looping over multiple page

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python3
 """This modeule  creates a method that returns
-list of ships that can hold a given number of passengers.
+the list of ships that can hold a given number of passengers.
 """
 import requests
 
 
 def availableShips(passengerCount):
-    """Returns the list of ships that can hold a given
-    number of passengers. Don't forget the pagination.
-    If no ship available, return an empty list.
+    """Returns the list of ships that can hold a given number of passengers.
     """
     available_ships = []
     url = "https://swapi-api.hbtn.io/api/starships/"
+
     while url:
-        response = requests.get()
-        # get and convert http response into json (dictionary)
+
+        response = requests.get(url)
+
         data = response.json()
-        # get only value 'of' keys in dictionary
+
         ships = data["results"]
 
         for ship in ships:
-            passengers = ship["passengers"]
-            passengers = passengers.replace(",", "")
-            if passengers.isdigit():
-                passengers = int(passengers)
-                if passengers >= passengerCount:
+            passerngers = ship["passengers"]
+
+            passerngers = passerngers.replace(",", "")
+            if passerngers.isdigit():
+                passerngers = int(passerngers)
+
+                if passerngers >= passengerCount:
                     available_ships.append(ship["name"])
+
         url = data["next"]
+
     return available_ships
