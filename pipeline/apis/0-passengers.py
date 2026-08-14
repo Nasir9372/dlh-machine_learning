44e3ed1a03a13e9d6055a529302f1049af5c74c2
nasirk007 <nsr.capital.007@outlook.com>
2026-08-14 05:01:03 +0200

    get method to get ships that can handle given number of passengers

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -10,25 +10,16 @@ def availableShips(passengerCount):
     """
     available_ships = []
     url = "https://swapi-api.hbtn.io/api/starships/"
-
     while url:
-
         response = requests.get(url)
-
         data = response.json()
-
         ships = data["results"]
-
         for ship in ships:
             passerngers = ship["passengers"]
-
             passerngers = passerngers.replace(",", "")
             if passerngers.isdigit():
                 passerngers = int(passerngers)
-
                 if passerngers >= passengerCount:
                     available_ships.append(ship["name"])
-
         url = data["next"]
-
     return available_ships
